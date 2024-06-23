1ee828c585798adc0208dc6239216822cf745d93
vlad1k337 <vladguy.baranovskii@gmail.com>
2024-06-23 11:06:38 -0400

    Imgui admin panel

--- antipendos.py
+++ antipendos.py
@@ -3,13 +3,17 @@ import sys
 from datetime import datetime
 import re
 import os
-
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message, FSInputFile
 from aiogram import Router
+import imgui
+import glfw
+import OpenGL.GL as gl
+from imgui.integrations.glfw import GlfwRenderer
+
 
 CAPITAL_BEGIN = 65
 CAPITAL_END = 90
@@ -62,6 +66,71 @@ async def message_handler(message: Message) -> None:
 async def main() -> None:
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+async def stop() -> None:
+    await dp.stop_polling(bot)
 
+def impl_glfw_init(window_name="minimal ImGui/GLFW3 example", width=1280, height=720):
+    if not glfw.init():
+        print("Could not initialize OpenGL context")
+        exit(1)
+
+    # OS X supports only forward-compatible core profiles from 3.2
+    glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, gl.GL_TRUE)
+
+    # Create a windowed mode window and its OpenGL context
+    window = glfw.create_window(int(width), int(height), window_name, None, None)
+    glfw.make_context_current(window)
+
+    if not window:
+        glfw.terminate()
+        print("Could not initialize Window")
+        exit(1)
+
+    return window
+
+
+class GUI(object):
+    def __init__(self):
+        super().__init__()
+        self.backgroundColor = (0, 0, 0, 1)
+        self.window = impl_glfw_init()
+        gl.glClearColor(*self.backgroundColor)
+        imgui.create_context()
+        self.impl = GlfwRenderer(self.window)
+
+        self.string = ""
+        self.f = 0.5
+
+        self.loop()
+
+    def loop(self):
+        while not glfw.window_should_close(self.window):
+            glfw.poll_events()
+            self.impl.process_inputs()
+            imgui.new_frame()
+            imgui.begin("Custom window", True)
+
+            if imgui.button("START"):
+                asyncio.run(main()) 
+            if imgui.button("STOP"):
+                asyncio.run(stop())
+
+            imgui.end()
+
+            imgui.render()
+
+            gl.glClearColor(*self.backgroundColor)
+            gl.glClear(gl.GL_COLOR_BUFFER_BIT)
+
+            self.impl.render(imgui.get_draw_data())
+            glfw.swap_buffers(self.window)
+
+        self.impl.shutdown()
+        glfw.terminate()
+
+if __name__ == "__main__":
+   gui = GUI()
